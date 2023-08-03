cf9eaa45d88da12a02e5c5c56d0cd1652495cbf8
Maximilian Wagner <maximilian.wagner@smail.inf.h-brs.de>
2023-08-03 11:35:34 +0200

    Created port variable to make it foolproof. You never know.

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from waitress import serve
 from app import create_app
 import os
 
+PORT = 5000
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-serve(create_app(), port=5000)
+serve(create_app(), port=PORT)
